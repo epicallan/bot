9ab248cb7e042ec63c9ff922bad71426b568fac7
Allan Lukwago <epicallan.al@gmail.com>
2017-05-04 10:14:58 +0300

    all most there with webhook module :smiley:

--- src/Messenger/Bot.purs
+++ src/Messenger/Bot.purs
@@ -1,22 +1,19 @@
 module Messenger.Bot where
-import Control.Bind ((>>=))
-import Control.Monad.Eff (foreachE)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (error, info, log)
+import Control.Monad.Eff.Console (error, info, log, warn)
 import Control.Monad.Except (runExcept)
-import Data.Array (head)
 import Data.Either (Either(..))
 import Data.Foldable (traverse_)
-import Data.Foreign (F, MultipleErrors)
-import Data.Foreign.Class (class IsForeign, readJSON)
+import Data.Foreign (F)
+import Data.Foreign.Class (readJSON)
 import Data.Foreign.NullOrUndefined (unNullOrUndefined)
 import Data.Maybe (Maybe(..), maybe)
 import Data.Tuple (Tuple(..))
-import Messenger.Model.MessageEvent (EventAction(..), MessageEffs, MessageEntry(..), MessageEvent(..), Messaging(..), Postback(..), Response(..), WebHookEffs)
+import Messenger.Model.MessageEvent (EventAction(..), MessageEffs, MessageEntry(..), MessageEvent(..), Messaging(..), Response(..))
+import Node.Express.Handler (HandlerM)
 import Node.Express.Request (getBody)
-import Node.Express.Response (setStatus)
-import Prelude (Unit, bind, const, join, map, pure, void, ($), (<>))
-import Utils (multpleErrorsToStr)
+import Node.Express.Types (EXPRESS)
+import Prelude (Unit, bind, join, map, pure,  ($))
 
 
 sendResponse :: forall e. Response -> MessageEffs e Unit
@@ -41,17 +38,27 @@ getEventAction (Messaging ms) =
             Nothing -> Nothing
 
 
-messageEventRunner :: forall e. (EventAction -> MessageEffs e (Maybe Response)) -> MessageEvent -> MessageEffs e Unit
-messageEventRunner handler (MessageEvent messageEvent@{ object, entry}) = do
-  let eventActions = join $ map (\(MessageEntry messageEntry@{ messaging }) -> map getEventAction messaging) entry
-  case (head eventActions) of
-    Nothing -> liftEff $ log "No event"
-    Just maybeAction -> do
-      case maybeAction of
-        Nothing -> liftEff $ log "No event"
-        Just event -> do
-          liftEff $ log "handle event action"
-          maybeRes <- handler event
-          case maybeRes of
-            Nothing ->  liftEff $ log "no Response"
-            Just res -> sendResponse res
+messageEventRunner :: forall e. (EventAction -> MessageEffs e (Maybe Response)) -> Either String MessageEvent-> MessageEffs e Unit
+messageEventRunner handler maybeMe  = do
+  case maybeMe of
+    Left err -> liftEff $ error $ "No event"
+    Right (MessageEvent messageEvent@{ object, entry}) -> do
+      let eventActions = join $ map (\(MessageEntry messageEntry@{ messaging }) -> map getEventAction messaging) entry
+      liftEff $ traverse_ (\eventAction ->
+          case eventAction of
+            Nothing    -> liftEff $ info "No event"
+            Just event -> do
+              maybeRes <- handler event
+              maybe (liftEff $ warn "no Response") sendResponse maybeRes
+              ) eventActions
+
+getMessageEvent :: forall e. HandlerM (express :: EXPRESS | e) (Either String MessageEvent)
+getMessageEvent = do
+  eitherBodyRaw <- getBody
+  case eitherBodyRaw of
+    Left multipleBodyErrors -> pure $ Left "error parsing request body"
+    Right body -> do
+      let eitherMessageEvent = runExcept $ readJSON body :: F MessageEvent
+      case eitherMessageEvent of
+        Left multipleJsonErrors -> pure $ Left "error decoding body to messageEvent"
+        Right messageEvent -> pure $ Right messageEvent
